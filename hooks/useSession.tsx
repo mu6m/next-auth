f69f4c7071ec1c5708605c2b5feeea1e6869a8e2
mu6m <theledraa@gmail.com>
2024-07-21 11:22:55 +0300

    session

--- hooks/useSession.tsx
+++ hooks/useSession.tsx
@@ -1,7 +1,35 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { cookies } from "next/headers";
+import { verifyAccessToken } from "@/lib/jwt";
 
-export const useSession = create((set) => ({
-	user: null,
-	setUser: (user: any) => set({ user }),
-	clearUser: () => set({ user: null }),
-}));
+interface AuthStore {
+	isLoggedIn: boolean;
+	login: () => void;
+	logout: () => void;
+}
+const useSession = create(
+	persist<AuthStore>(
+		(set: any) => ({
+			isLoggedIn: false,
+			login: async () => {
+				const cookie = await cookies().get("user");
+				const token = await verifyAccessToken(cookie?.value);
+				if (token === false || !token) {
+					await cookies().delete("user");
+					return set({ isLoggedIn: false });
+				}
+				return set({ isLoggedIn: true });
+			},
+			logout: async () => {
+				await cookies().delete("user");
+				return set({ isLoggedIn: false });
+			},
+		}),
+		{
+			name: "userLoginStatus",
+		}
+	)
+);
+
+export default useSession;
